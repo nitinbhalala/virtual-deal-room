3c6fc9bebad125f036a2c936864cac266da404dd
Nitin Bhalala <bhalalanitin5291@gmail.com>
2025-04-11 12:24:31 +0530

    Update socket.ts

--- vertxai_frontend/src/utils/socket.ts
+++ vertxai_frontend/src/utils/socket.ts
@@ -6,6 +6,10 @@ console.log("SOCKET_URL", SOCKET_URL);
 // const SOCKET_URL ='ws://192.168.29.75:4000'; 
 
 export const socket: Socket = io(SOCKET_URL, {
+  transports: ['websocket'],
   autoConnect: false, // we'll manually connect it
+  timeout: 60000, // 60 seconds
+  reconnectionAttempts: 5,
+  reconnectionDelay: 5000
 });
 
