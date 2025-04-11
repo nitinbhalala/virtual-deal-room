2b1e9983ea06d8cea832ac1584041d7a20adcc3f
Nitin Bhalala <bhalalanitin5291@gmail.com>
2025-04-11 12:11:53 +0530

    Update socket.ts

--- vertxai_frontend/src/utils/socket.ts
+++ vertxai_frontend/src/utils/socket.ts
@@ -2,6 +2,7 @@
 import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL = process.env.VITE_WS_BASE_URL
+console.log("SOCKET_URL", SOCKET_URL);
 // const SOCKET_URL ='ws://192.168.29.75:4000'; 
 
 export const socket: Socket = io(SOCKET_URL, {
